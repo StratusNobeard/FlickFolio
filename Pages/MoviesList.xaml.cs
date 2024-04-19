79a637ea95b4aaa42874ef49e322647bf5ba834d
Korisnik <Korisnik@STRATUS-DESKTOP>
2024-04-19 18:26:09 +0200

    bub

---
+++ Pages/MoviesList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FlickFolio.Pages
+{
+    /// <summary>
+    /// Interaction logic for MovieListPage.xaml
+    /// </summary>
+    public partial class MoviesList : Page
+    {
+        public MoviesList()
+        {
+            InitializeComponent();
+        }
+    }
+}
